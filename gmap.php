87cbb8b912290cb41cb35d67600ef29b011da0a4
mm <mmm@zzz.com>
2020-02-05 23:10:08 +0900

    緯度、経度をstr -> float化。

--- gmap.php
+++ gmap.php
@@ -131,7 +131,8 @@
       //}
      function initMap() {
        // 地図の作成
-       var mapLatLng = new google.maps.LatLng({lat: markerData2[0]['lat'], lng: markerData2[0]['lng']}); // 緯度経度のデータ作成
+       var mapLatLng = new google.maps.LatLng({lat: parseFloat(markerData2[0]['lat']),
+                                                lng: parseFloat(markerData2[0]['lng'])}); // 緯度経度のデータ作成
        map = new google.maps.Map(document.getElementById('map'), { // #mapに地図を埋め込む
          center: mapLatLng, // 地図の中心を指定
          zoom: 15 // 地図のズームを指定
@@ -139,7 +140,8 @@
  
        // マーカー毎の処理
        for (var i = 0; i < markerData2.length; i++) {
-          markerLatLng = new google.maps.LatLng({lat: markerData2[i]['lat'], lng: markerData2[i]['lng']}); // 緯度経度のデータ作成
+          markerLatLng = new google.maps.LatLng({lat: parseFloat(markerData2[i]['lat']),
+                                                    lng: parseFloat(markerData2[i]['lng'])}); // 緯度経度のデータ作成
           marker[i] = new google.maps.Marker({ // マーカーの追加
             position: markerLatLng, // マーカーを立てる位置を指定
             map: map // マーカーを立てる地図を指定

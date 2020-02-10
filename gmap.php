c4abca88fdd4186efa914f953a9d3c5cce1c7a2c
mm <mmm@zzz.com>
2020-02-10 10:16:11 +0900

    marker iconをgroupによって変更。zxyは緑、workstylingは青。

--- gmap.php
+++ gmap.php
@@ -152,9 +152,18 @@
         for (var i = 0; i < markerData2.length; i++) {
             markerLatLng = new google.maps.LatLng({lat: parseFloat(markerData2[i]['lat']),
                                                     lng: parseFloat(markerData2[i]['lng'])}); // 緯度経度のデータ作成
+            // マーカーiconを決める
+            if(markerData2[i]['group'] == 'zxy'){
+                markericon = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+            }else if(markerData2[i]['group'] == 'workstyling'){
+                markericon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+            }else{
+                markericon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+            }
             marker[i] = new google.maps.Marker({ // マーカーの追加
                 position: markerLatLng, // マーカーを立てる位置を指定
-                map: map // マーカーを立てる地図を指定
+                map: map,  // マーカーを立てる地図を指定
+                icon: markericon
             });
             infoWindow[i] = new google.maps.InfoWindow({ // 吹き出しの追加
                 content: '<div class="map">' + markerData2[i]['title'] + '</div>' // 吹き出しに表示する内容

d21d2909f70a6e4e06afbad9f9ea54b425c97870
mm <mmm@zzz.com>
2020-02-10 17:22:14 +0900

    self.parse_newwork()になんでか飛んでくれない。
    parse_newwork()ないでは、
    　googlemapのshort urlをlong urlに戻して、そこからlat/lngを抽出する予定である。

--- crawler/spiders/example.py
+++ crawler/spiders/example.py
@@ -3,18 +3,28 @@ import scrapy
 from crawler.items import Post
 import googlemaps  # pip install googlemap
 import time
-
+import requests # conda install requests
 
 ZXY_DOMAIN = 'zxy.work'
 WORKSTYLYING_DOMAIN = 'workstyling.jp'
+NEWWORK_DOMEIN = 'newwork109.com'
 APIKEYFILE='apikey.txt'
 GOOGLEAPIKEY = ''
 
 
+#
+# short URLを元に戻す
+#
+def unshorten_url(url):
+    session = requests.Session()  # so connections are recycled
+    resp = session.head(url, allow_redirects=True)
+    return resp.url
+
 class ExampleSpider(scrapy.Spider):
     name = 'zxy'
     allowed_domains = [ZXY_DOMAIN, WORKSTYLYING_DOMAIN]
-    start_urls = ['https://zxy.work/', 'https://mf.workstyling.jp/share/']
+    # start_urls = ['https://zxy.work/', 'https://mf.workstyling.jp/share/', 'https://www.newwork109.com/post/station']
+    start_urls = ['https://www.newwork109.com/post/station']
 
     #
     # goole api keyを読み込む
@@ -51,8 +61,6 @@ class ExampleSpider(scrapy.Spider):
     # WorkStylyingの店舗ページのパース
     #
     def parse_workstyling(self, response):
-
-
         item = Post()
         item['url'] = response.url
         item['title'] = response.css('h1.pagename__ttl::text').extract_first()
@@ -74,6 +82,35 @@ class ExampleSpider(scrapy.Spider):
 
         item['group'] = "workstyling"
         yield item
+
+    #
+    # NewWorkの店舗ページのパース
+    #
+    def parse_newwork(self, response):
+        item = Post()
+        item['url'] = response.url
+        item['title'] = response.css('h2.store-name::text').extract_first()
+        item['desc'] = response.css('meta[name="description"]::attr("content")').extract_first()
+
+        shortURL = response.css('div.google-btn a::text').extract_first()
+        longURL = unshorten_url(shortURL)
+        # item['address'] = response.css('.googleMap::attr("data-office-address")').extract_first()
+        dts = response.css('div.p-share-office-access__detail dl dt::text').extract()
+        dds = response.css('div.p-share-office-access__detail dl dd::text').extract()
+        for dt, dd in zip(dts, dds):
+            if dt == '住所':
+                item['address'] = dd
+                break
+
+        gmaps = googlemaps.Client(key=GOOGLEAPIKEY)
+        result = gmaps.geocode(item['address'])
+        item['lat'] = result[0]["geometry"]["location"]["lat"]
+        item['lng'] = result[0]["geometry"]["location"]["lng"]
+        time.sleep(0.5)  # 連続してgeocodeを呼ぶと失敗するという噂。
+
+        item['group'] = "newwork"
+        yield item
+
 
     #
     # TOPページのparse
@@ -102,6 +139,19 @@ class ExampleSpider(scrapy.Spider):
                 print("--WS   {}".format(url))
                 yield scrapy.Request(url, self.parse_workstyling)
 
+        elif NEWWORK_DOMEIN in response.url:
+            print('拠点一覧(NEW WORK)')
+            '''
+            <div id="store-top">
+			    <ul class="store">
+				<!-- リンクはリスト毎 -->
+				<a href="/post/store/1636">
+            '''
+            links = response.css('div#store-top > ul.store > a::attr("href")').extract()
+            for link in links:
+                url = response.urljoin(link)
+                print("--NW   {}".format(url))
+                yield scrapy.Request(url, self.parse_newwork)
         else:
             pass
 

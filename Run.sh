cd6180a1ac3dff913a7c85b9c67029fe0bf1177c
mm <mmm@zzz.com>
2020-02-19 21:30:18 +0900

    クロール後に、前回のクロールと今回のクロールで新しい店舗が見つかったり、なくなった店舗がないか確認する。

--- Run.sh
+++ Run.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 #
 #
-if [ -f zxy.json ] ; then rm zxy.json ; fi
-scrapy crawl zxy -o zxy.json
+OLDFILE=old_base.json
+NEWFILE=latest_base.json
+NEWBASE=appear.json
+DISAPPER=disappear.json
+
+if [ -f $NEWFILE ] ; then mv $NEWFILE $OLDFILE ; fi
+scrapy crawl zxy -o $NEWFILE
+python json_diff.py $OLDFILE $NEWFILE $NEWBASE $DISAPPER
+
+echo "NEW BASE=========================================="
+cat $NEWBASE
+
+echo ""
+echo "DISAPPER BASE=========================================="
+cat $DISAPPER

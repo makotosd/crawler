4ff90222b100d9e47bf1263a4de37770ddcd3ab2
mm <mmm@zzz.com>
2020-02-22 21:53:23 +0900

    同じ住所に二軒の店舗があった場合、後の方を0.001度ずらした。 三軒以上重なっていた場合、二軒目と三軒目が同じ場所にきちゃう。 pipelineに実装した。

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -7,5 +7,15 @@
 
 
 class CrawlerPipeline(object):
+    latlng = {}
+
     def process_item(self, item, spider):
+        s = str(item['lat']) + "_" + str(item['lng'])
+        if s in self.latlng:
+            item['lat'] += 0.001
+            print('location of "{}" is overlaped with "{}"'.format(item['title'], self.latlng[s]))
+        else:
+            pass
+        self.latlng[s] = item['title']
+
         return item

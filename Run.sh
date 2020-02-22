f2dd36b38fa240fba721c0c8df7ad27b75634260
mm <mmm@zzz.com>
2020-02-22 22:37:51 +0900

    scrapyのlog残し。

--- Run.sh
+++ Run.sh
@@ -7,7 +7,7 @@ NEWBASE=appear.json
 DISAPPER=disappear.json
 
 if [ -f $NEWFILE ] ; then mv $NEWFILE $OLDFILE ; fi
-scrapy crawl zxy -o $NEWFILE
+scrapy crawl zxy -o $NEWFILE 2>&1 | tee log.scrapy
 python json_diff.py $OLDFILE $NEWFILE $NEWBASE $DISAPPER
 
 echo "NEW BASE=========================================="

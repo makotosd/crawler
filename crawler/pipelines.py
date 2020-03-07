a8fbbc8ae95cdb41f74b2b556ff3fcaaca5245bd
mm <mmm@zzz.com>
2020-03-07 13:32:59 +0900

    pipeline側にmysqlとの接続の枠組みを実装。クエリはまだ空っぽ。

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -5,6 +5,10 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from urllib.parse import urlparse
+import mysql.connector  # need to install mysql-connector-python
+import pandas.io.sql as psql  # need to install pandas
+
 
 class CrawlerPipeline(object):
     latlng = {}
@@ -12,14 +16,29 @@ class CrawlerPipeline(object):
     # コンストラクタ
     def __init__(self):
         # DBとの接続
-        pass
+        url = urlparse('mysql://root:root@127.0.0.1:13306/shop_database')  # for Ops
+        self.conn = mysql.connector.connect(
+            host=url.hostname,
+            port=url.port,
+            user=url.username,
+            database=url.path[1:],
+            password=url.password
+        )
 
     # デストラクタ
     def __del__(self):
         # DBとの切断
-        pass
+        self.conn.commit()
+        self.conn.close()
+
 
     def process_item(self, item, spider):
+        url = item['url']
+        title = item['title']
+        sql = ""  # select * from shop_info;"  # TODO: urlとtitleでSQLからselectする。
+        result_mysql = psql.execute(sql, self.conn)
+        record = result_mysql.fetchone()
+        print(url, title, record)
         '''
         s = str(item['lat']) + "_" + str(item['lng'])
         if s in self.latlng:  # 重なっていたら0.0001/0.0001度ずらす。

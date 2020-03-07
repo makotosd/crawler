9d728376328c70a890cf7b7d46027922322a88c6
mm <mmm@zzz.com>
2020-03-07 10:04:03 +0900

    jsonをmysqlに全部入れた。newworkはaddressが入っていないようだ。

--- put_json_to_mysql.py
+++ put_json_to_mysql.py
@@ -16,11 +16,12 @@ import json
 
 JSONFILE = 'latest_base.json'
 
+conn = ''
 
 def createDB():
     url = urlparse('mysql://root:root@127.0.0.1:13306/shop_database')  # for Ops
     # url = urlparse('mysql+pymysql://stock@localhost:3306/stockdb')  # for Dev
-
+    global conn
     conn = mysql.connector.connect(
         host=url.hostname,
         port=url.port,
@@ -33,15 +34,15 @@ def createDB():
     # table 作成
     ############################################################
     sql = "CREATE TABLE IF NOT EXISTS shop_info (" \
-          "url char(255) NOT NULL," \
-          "title char(128) NOT NULL," \
-          "description char(255)," \
-          "address char(255)," \
+          "url varchar(255) NOT NULL," \
+          "title varchar(128) NOT NULL," \
+          "description varchar(255)," \
+          "address varchar(255)," \
           "lat double," \
           "lng double," \
           "first_entry datetime NOT NULL," \
           "last_update datetime NOT NULL," \
-          "shop_group char(64) NOT NULL," \
+          "shop_group varchar(64) NOT NULL," \
           "PRIMARY KEY(title, url)" \
           ")"
 
@@ -50,24 +51,38 @@ def createDB():
 
 
 def readJson():
+    global conn
+
+    now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+
     with open(JSONFILE) as f:
         df = json.load(f)
-    for shop_info in df:
-        print(shop_info)
-        for key in shop_info:
-            print("  {} {}".format(key, shop_info[key]))
+        for shop_info in df:
+            print(shop_info)
+
+            if 'address' not in shop_info:
+                shop_info['address'] = ""
+
+            keys = ",".join(['url', 'title', 'description', 'address', 'lat', 'lng',
+                             'first_entry', 'last_update', 'shop_group'])
+            values = '"%s","%s","%s", "%s", %f, %f, "%s", "%s", "%s"' % (
+                        shop_info['url'], shop_info['title'], shop_info['desc'], shop_info['address'],
+                        shop_info['lat'], shop_info['lng'], now, now, shop_info['group'])
+            sql = "INSERT INTO {} ({}) VALUES ({});".format("shop_info", keys, values)
+            print(sql)
+            psql.execute(sql, conn)
+            conn.commit()
 
 
 def closeDB():
-    pass
+    global conn
+    conn.close()
 
 
 def do_main():
     createDB()
     readJson()
     closeDB()
-
-
 
 
 if __name__ == "__main__":

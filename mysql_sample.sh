ec2b926262e23981c4b534e817b5d79f88dc87f9
mm <mmm@zzz.com>
2020-03-07 09:16:51 +0900

    mysqlのメモを追加。

--- mysql_sample.sh
+++ mysql_sample.sh
@@ -5,3 +5,4 @@ mysql --user=root --password=root --host=127.0.0.1 --port=13306 shop_database
 
 # mysql> show columns from shop_info;
 # mysql> select * from shop_info;
+# mysql> drop table shop_info;
